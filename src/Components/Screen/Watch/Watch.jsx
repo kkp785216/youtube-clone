1b5102dbc532333486ab0213973d6affe9094ae8
Krishna Kumar <kkp785216@gmail.com>
2022-07-17 17:44:17 +0530

    watch screen improved

--- src/Components/Screen/Watch/Watch.jsx
+++ src/Components/Screen/Watch/Watch.jsx
@@ -1,16 +1,70 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import './Watch.scss'
 import { useSearchParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getVideosByCategory, getVideosList } from '../../../Redux/Actions/video.action'
+import WatchSidebar from './WatchSidebar/WatchSidebar'
+import request from '../../../Database/Api'
 
 const Watch = () => {
-
   let [searchParams, setSearchParams] = useSearchParams();
+  let [iframeLoad, setIframeLoad] = useState(false);
+
+  const dispatch = useDispatch();
+  const { videos, loading, moreDetails, channelDetails, videoCategory } = useSelector(state => state.homeVideos);
+  const { activeCategory } = useSelector(state => state.catgoryState);
+  const [currentVideo, setCurrentVideo] = useState(null);
+
+  useEffect(() => {
+    let videoDetails = { video: {}, channel: {} };
+    const res = request("/videos", {
+      params: {
+        part: "snippet,contentDetails,statistics",
+        id: searchParams.get('v')
+      }
+    });
+    res.then(data => {
+      let video = data.data.items[0];
+      videoDetails.video = video;
+      const get_channel_details = request("/channels", {
+        params: {
+          part: 'snippet',
+          id: video.snippet.channelId
+        }
+      });
+      get_channel_details.then(cdata => {
+        let channel = cdata.data.items[0];
+        videoDetails.channel = channel;
+        setCurrentVideo(videoDetails);
+        console.log(videoDetails)
+      })
+    });
+  }, [searchParams]);
+
+  useEffect(() => {
+    if (videos.length === 0 || videoCategory !== activeCategory) {
+      activeCategory === 'All' ? dispatch(getVideosList(activeCategory, 20)) : dispatch(getVideosByCategory(activeCategory, 20));
+    }
+  }, [dispatch, activeCategory, videoCategory, videos.length]);
+
+  const publishedAt = (input) => {
+    let date = new Date(input);
+    let a = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()] + ' ' + date.getDay() + ' ' + date.getFullYear()
+    return a;
+  }
+
+  const setDescription = (input) => {
+    try{
+      document.querySelector('.description').innerText = input;
+    }catch{}
+  }
 
   return (
     <div className="row">
       <div className="col-8">
         <div className="watch-screen-player">
           <iframe
+            onLoad={() => { setIframeLoad(true) }}
             src={`https://www.youtube.com/embed/${searchParams.get('v')}?autoplay=1`}
             title='Youtube Watch'
             frameBorder="0"
@@ -18,11 +72,26 @@ const Watch = () => {
             width='100%'
             id="youtube-player-screen"
             className='iframe'
-            style={{ aspectRatio: '1280/720' }}
           ></iframe>
         </div>
+        {currentVideo && iframeLoad && <>
+          <h3>{currentVideo.video.snippet.title}</h3>
+          <span>{currentVideo.video.statistics.viewCount} views</span>
+          <span> • </span>
+          <span>{publishedAt(currentVideo.video.snippet.publishedAt)}</span>
+          <p className='description'>{setDescription(currentVideo.video.snippet.description)}</p>
+        </>}
+
       </div>
-      <div className="col-4"></div>
+      <div className="col-4">
+        {iframeLoad && iframeLoad &&
+          videos.map((videos, index) => (
+            <div className='col' key={index}>
+              <WatchSidebar videos={videos} loading={loading} moreDetails={moreDetails[index]} channelDetails={channelDetails[index]} />
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
---
+++ src/Components/Screen/Watch/WatchSidebar/WatchSidebar.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import './WatchSidebar.scss'
+import moment from 'moment'
+import { Link } from 'react-router-dom'
+
+const WatchSidebar = ({ videos, moreDetails, channelDetails }) => {
+
+  const count = (input) => {
+    if (input < 1000) {
+      return input
+    }
+    else if (input < 1000000) {
+      return parseInt(input / 1000) + "K"
+    }
+    else if (input < 1000000000) {
+      return parseInt(input / 1000000) + "M"
+    }
+    else if (input < 1000000000000) {
+      return parseInt(input / 1000000000) + "B"
+    }
+  }
+
+  function duration(input) {
+    const seconds = moment.duration(input).asSeconds();
+    const _duration = moment.utc(seconds * 1000).format("mm:ss");
+    return _duration;
+  }
+
+  // Use more video detals
+  const [videoDetails, setVideoDetails] = useState(null);
+  useEffect(() => {
+    moreDetails.then((res) => {
+      setVideoDetails(res.data.items[0])
+    });
+  }, [moreDetails]);
+
+  // fetch channel icons
+  const [channel, setChannel] = useState({ channelImg: '', id: '' });
+  useEffect((channel) => {
+    channelDetails.then((res) => {
+      setChannel({
+        ...channel,
+        channelImg: res.data.items[0].snippet.thumbnails.default.url,
+        id: res.data.items[0].id
+      });
+    });
+  }, [channelDetails]);
+
+  return (
+    <div className="video watch-video">
+      <Link to={`/watch?v=${videos ? videos.id.videoId === undefined ? videos.id : videos.id.videoId : ''}`} className="video__top d-block">
+        <img src={videos.snippet.thumbnails.high.url} width="100%" alt="" />
+        <span>{videoDetails && duration(videoDetails.contentDetails.duration)}</span>
+      </Link>
+
+      <div className="video__details">
+        <Link to={`/c/${channel.id}`} className="video__channel">
+          <img src={channel.channelImg} width="33px" alt="" />
+        </Link>
+
+        <div className="video__title">
+          <h3><Link to={`/watch?v=${videos ? videos.id.videoId === undefined ? videos.id : videos.id.videoId : ''}`}>{videos.snippet.title}</Link></h3>
+          <div className='video__title__info'>
+            <span><Link to={`/c/${channel.id}`} className='channel__title'>{videos.snippet.channelTitle}</Link></span>
+            <Link to={`/watch?v=${videos ? videos.id.videoId === undefined ? videos.id : videos.id.videoId : ''}`}>
+              <span>{count(videoDetails && videoDetails.statistics.viewCount) + " views"}</span>
+              <span> • </span>
+              <span>{moment(videos.snippet.publishedAt).fromNow()}</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WatchSidebar;
