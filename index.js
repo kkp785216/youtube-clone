5aa12f8c223b074c2bb77c8d7f87b542417cf04c
Krishna Kumar <kkp785216@gmail.com>
2022-07-23 23:55:12 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import express from 'express'
 import youtubesearchapi from 'youtube-search-api'
+import cors from 'cors'
 const app = express()
-const port = 3000 || process.env.port
+const port = 5500 || process.env.port
+
+app.use(cors());
 
 app.get('/', (req, res) => {
-    let response = youtubesearchapi.GetListByKeyword(req.query.q, false, 20)
+    let maxResult = req.query.maxResult !== undefined ? req.query.maxResult : 20;
+    let response = youtubesearchapi.GetListByKeyword(req.query.q, false, maxResult)
     response.then(data => {
         res.send(data);
     })
 })
 
 app.get('/next', (req, res) => {
+    let maxResult = req.query.maxResult !== undefined ? req.query.maxResult : 20;
     let objs = JSON.parse(req.query.nextPage)
-    let response = youtubesearchapi.NextPage(objs, false, 20)
+    let response = youtubesearchapi.NextPage(objs, false, maxResult)
     response.then(data => {
         res.send(data);
     })
