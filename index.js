58b15758043322d74e6c651c36e8ed89ae284f0f
Krishna Kumar <kkp785216@gmail.com>
2022-07-23 15:11:36 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from 'express'
 import youtubesearchapi from 'youtube-search-api'
 const app = express()
-const port = 3000
+const port = 3000 || process.env.port
 
 app.get('/', (req, res) => {
-    let response = youtubesearchapi.GetListByKeyword("radhe radhe", false, 20)
-    response.then(data=>{
-        let response = youtubesearchapi.NextPage(data.nextPage,false,20)
-        response.then(data=>{
-            res.send(data);
-
-        })
+    let response = youtubesearchapi.GetListByKeyword(req.query.q, false, 20)
+    response.then(data => {
+        res.send(data);
     })
 })
+
+app.get('/next', (req, res) => {
+    let objs = JSON.parse(req.query.nextPage)
+    let response = youtubesearchapi.NextPage(objs, false, 20)
+    response.then(data => {
+        res.send(data);
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
